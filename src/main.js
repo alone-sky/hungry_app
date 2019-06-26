278531000ba43bfb75d60ad7ca3225305c628a58
luwb <18270895950@163.com>
2019-06-26 10:43:29 +0800

    安装vuex状态管理

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store/index'
 import { LoadingPlugin } from 'vux'
 import { AlertPlugin } from 'vux'
 import Element from 'element-ui'
@@ -12,10 +13,11 @@ Vue.config.productionTip = false
 Vue.use(Element)
 Vue.use(AlertPlugin)
 Vue.use(LoadingPlugin)
-/* eslint-disable no-new */
+
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
